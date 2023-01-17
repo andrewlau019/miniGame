548a3a77bf792ed5d0ce8d33939b5ef6d3477ae3
Andrew Lau Ho YIn <70436793+andrewlau019@users.noreply.github.com>
2023-01-16 19:11:14 -0800

    Updated the first step
    put the image onto canvas

---
+++ player.js
@@ -0,0 +1,13 @@
+class Player {
+    constructor() {
+
+    };
+
+    update() {
+
+    };
+
+    draw() {
+        ctx.drawImage(ASSET_MANAGER.getAsset("./sprite/sprite_sample1.png"), 0, 0);
+    };
+}
