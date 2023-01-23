357a10bc5fd92f4ff7f0b7aaf77765dc1432f225
Andrew Lau Ho YIn <70436793+andrewlau019@users.noreply.github.com>
2023-01-22 16:13:58 -0800

    make the player move

--- player.js
+++ player.js
@@ -2,14 +2,19 @@ class Player {
     constructor(game) {
         this.game = game;
         this.animator = new Animator(ASSET_MANAGER.getAsset("./jjk run.png"), 0, 0, 452/8, 51, 8, 0.2);
+
+        this.x = 0;
+        this.y = 0 ;
+        this.speed = 210;
     };
 
     update() {
-
+        this.x += this.speed * this.game.clockTick;
+        if(this.x > 1024) this.x = 0;
     };
 
     draw(ctx) {
         //ctx.drawImage(ASSET_MANAGER.getAsset("./jjk.png"), 0, 0);
-        this.animator.drawFrame(this.game.clockTick, ctx, 25, 25);
+        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
     };
 }
